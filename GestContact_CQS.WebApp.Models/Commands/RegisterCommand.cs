dda270e397757f474c948a2c28b450090b8d8959
Tyler Daerden <daerdentyler@gmail.com>
2023-06-28 14:23:49 +0200

    MAJ DB ajour contacts

--- GestContact_CQS.WebApp.Models/Commands/RegisterCommand.cs
+++ GestContact_CQS.WebApp.Models/Commands/RegisterCommand.cs
@@ -7,9 +7,30 @@ using System.Windows.Input;
 
 namespace GestContact_CQS.WebApp.Models.Commands
 {
-    public class RegisterCommand : 
+    public class RegisterCommand : ICommand
     {
+        public string Nom { get; init; }
+        public string Prenom { get; init; }
+        public string Email { get; init; }
+        public string Passwd { get; init; }
+        public RegisterCommand(string nom, string prenom, string email, string passwd)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Email = email;
+            Passwd = passwd;
+        }
 
+        public event EventHandler? CanExecuteChanged;
 
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

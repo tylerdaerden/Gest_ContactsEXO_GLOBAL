aa0e39e36d1087823ef9410eab93b54bd2c3f642
Tyler Daerden <daerdentyler@gmail.com>
2023-06-28 12:13:29 +0200

    Mise en Place LOGIN CSP

---
+++ Gest_ContactsDatabase/Procedures/CSP_Login.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[CSP_Login]
+	@Email NVARCHAR(384),
+	@Passwd NVARCHAR(20)
+AS
+BEGIN
+	SELECT Id, Nom, Prenom, Email 
+	FROM Utilisateur
+	WHERE	Email = @Email 
+	AND		Passwd = HASHBYTES('SHA2_512', @Passwd);
+	RETURN 0
+END
